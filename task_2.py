4150b3182d4fa08a81ff30a8cce5e1858fddded4
bukvaq <shamshurin.mak@yandex.ru>
2023-04-17 04:49:46 +0300

    Улучшение графиков

--- task_2.py
+++ task_2.py
@@ -8,8 +8,8 @@ def text_cutter(text):
     """Разбивает входной текст на строки"""
 
     text_split = text.split()
-    total_string = ""
-    for i in text_split:
+    total_string = text_split[0]
+    for i in text_split[1:]:
         if len(total_string.split("\n")[-1])+len(i)+1 >= 15:
             total_string = f"{total_string}\n{i}"
         else:
@@ -39,29 +39,35 @@ def main():
         for cluster_iteration in df_area.cluster_name.unique():
             df_cluster = df_area[df_area.cluster_name == cluster_iteration]
             df_cluster.plot.scatter(
-                x='x',
-                y='y',
+                x="x",
+                y="y",
                 ax=ax,
                 c=df_cluster.colors.tolist(),
                 label=cluster_iteration,
                 marker="o",
                 edgecolor="black",
-                linewidths=0.5
+                linewidths=0.5,
+                s=df_cluster["count"]/10+6,
+                alpha=0.8
             )
 
             for _, row_series in df_cluster.iterrows():
                 texts_to_adjust.append(
                     ax.annotate(text=text_cutter(row_series.keyword),
                                 xy=(row_series.x, row_series.y),
-                                # arrowprops={"arrowstyle": "->"},
-                                fontsize=7)
+                                fontsize=8)
                 )
+            for texts in texts_to_adjust:
+                texts.set_alpha(.8)
 
         ax.grid(False)
         ax.axis('off')
         ax.set_title(f"Area: {area_iteration}", y=-0.1)
 
         lgd = ax.legend(bbox_to_anchor=(-0.05, 0.5))
+
+        for handle in lgd.legendHandles:
+            handle.set_sizes([20.0])
 
         adjust_text(texts_to_adjust)
 
